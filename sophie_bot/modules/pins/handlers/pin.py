5e79f859f900bb1a931661d61358f95e595bd518
yacha <yacha@orangefox.tech>
2026-02-19 15:55:57 +0100

    Flatten argument access structure in PinHandler
    Arguments are now accessed directly from self.data rather than
    through self.data["args"].

--- sophie_bot/modules/pins/handlers/pin.py
+++ sophie_bot/modules/pins/handlers/pin.py
@@ -6,8 +6,8 @@ from ass_tg.types import EqualsArg, OptionalArg
 
 from sophie_bot.filters.admin_rights import BotHasPermissions, UserRestricting
 from sophie_bot.filters.cmd import CMDFilter
-from sophie_bot.utils.handlers import SophieMessageHandler
 from sophie_bot.services.bot import bot
+from sophie_bot.utils.handlers import SophieMessageHandler
 from sophie_bot.utils.i18n import gettext as _
 from sophie_bot.utils.i18n import lazy_gettext as l_
 
@@ -35,8 +35,10 @@ class PinHandler(SophieMessageHandler):
             await message.reply(_("You need to reply to a message to pin it."))
             return
 
+        self.handler_args
+
         # Determine notification (silent by default unless loud/notify is used)
-        loud = self.data["args"]["loud"] or self.data["args"]["notify"]
+        loud = self.data["loud"] or self.data["notify"]
         disable_notification = not loud
 
         await bot.pin_chat_message(
